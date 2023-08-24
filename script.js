6bc72e1c472323b9782f4f41bd31b4c9c3381532
Miwon <ayman.shade99@gmail.com>
2023-08-24 15:45:32 +0300

    deleted some imgs

--- script.js
+++ script.js
@@ -18,27 +18,26 @@ GryffindorButton.addEventListener('mousedown', () => {
     container.classList.remove(currentHouse);
     container.classList.add('gryffindor');
     currentHouse = 'gryffindor';
-}
-);HufflepuffButton.addEventListener('mousedown', () => {
+});
+HufflepuffButton.addEventListener('mousedown', () => {
     container.classList.remove(currentHouse);
     container.classList.add('Hufflepuff');
     currentHouse = 'Hufflepuff';
-}
-);RavenclawButton.addEventListener('mousedown', () => {
+});
+RavenclawButton.addEventListener('mousedown', () => {
     container.classList.remove(currentHouse);
     container.classList.add('ravinclaw');
     currentHouse = 'ravinclaw';
-}
-);SlytherinButton.addEventListener('mousedown', () => {
+});
+SlytherinButton.addEventListener('mousedown', () => {
     container.classList.remove(currentHouse);
     container.classList.add('Slytherin');
     currentHouse = 'Slytherin';
-}
-);
-
-container.addEventListener('transitionend', function(e) {
-  console.log('Look I didz a transition!!!111');
 });
+
+// container.addEventListener('transitionend', function (e) {
+//     console.log('Look I didz a transition!!!111');
+// });
 
 
 //you can make a currentHouse var that disaple the old class and gets updated with eatch click 
