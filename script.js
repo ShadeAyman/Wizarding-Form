09eadb7013f30d321ba92ca3b04a99eb1dfe7726
Miwon <ayman.shade99@gmail.com>
2023-09-02 12:15:34 +0300

    LOTS OF UPDATES

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const SlytherinButton = document.getElementById("SlytherinButton");
 const body = document.querySelector('body');
 const password = document.getElementById("password");
 const conPassword = document.getElementById("con-password");
+const error = document.querySelector('.error');
+const passwords = document.querySelectorAll(".password")
+const hidden = document.querySelector(".hidden");
 // const firstname = document.getElementById("First-Name")
 //defult values 
 let currentHouse = "gryffindor";
@@ -18,38 +21,74 @@ function houseChanger(housename) {
     body.classList.remove(currentHouse);
     body.classList.add(housename);
     currentHouse = housename;
+    hidden.value=housename;
 }
 GryffindorButton.addEventListener('mousedown', () => houseChanger("gryffindor"));
 HufflepuffButton.addEventListener('mousedown', () => houseChanger('Hufflepuff'));
 RavenclawButton.addEventListener('mousedown', () => houseChanger('ravinclaw'));
 SlytherinButton.addEventListener('mousedown', () => houseChanger('Slytherin'));
 
-conPassword.addEventListener('blur', () => {
-    let ps1 = password.value;
-    let ps2 = conPassword.value;
-    let flag=0;
 
-    if (ps1.length != ps2.length) {
-        console.log("broken");
-    }
-    else {
-        for (let i = 0; i < ps1.length; i++) {
-            if (ps1[i] != ps2[i]) {
-                flag++;
+passwords.forEach(function(element){
+    element.addEventListener('blur', () => {
+        let ps1 = password.value;
+        let ps2 = conPassword.value;
+        let flag = 0;
+        if (ps1.length != ps2.length) {
+            console.log("broken");
+            error.innerText = "PASSWORD MUST BE THE SAME LENGTH"
+        }
+        else {
+            for (let i = 0; i < ps1.length; i++) {
+                if (ps1[i] != ps2[i]) {
+                    flag++;
+                };
             };
-        };
-        if(flag===0)
-        {
-            console.log("works");
+            if(flag===0)
+            {
+                console.log("works");
+                error.innerText=""
+    
+            }
+            if (flag !== 0) {
+                console.log("not the same")
+                error.innerText = "PASSWORDS DON'T MATCH"
+    
+            }
         }
-        if(flag!==0)
-        {
-            console.log("not the same")
-        }
-    }
-    console.log(ps1);
-    console.log(ps2);
+        console.log(ps1);
+        console.log(ps2);
+    })
 })
+// conPassword.addEventListener('blur', () => {
+//     let ps1 = password.value;
+//     let ps2 = conPassword.value;
+//     let flag = 0;
+//     if (ps1.length != ps2.length) {
+//         console.log("broken");
+//         error.innerText = "PASSWORD MUST BE THE SAME LENGTH"
+//     }
+//     else {
+//         for (let i = 0; i < ps1.length; i++) {
+//             if (ps1[i] != ps2[i]) {
+//                 flag++;
+//             };
+//         };
+//         if(flag===0)
+//         {
+//             console.log("works");
+//             error.innerText=""
+
+//         }
+//         if (flag !== 0) {
+//             console.log("not the same")
+//             error.innerText = "PASSWORDS DON'T MATCH"
+
+//         }
+//     }
+//     console.log(ps1);
+//     console.log(ps2);
+// })
 
 // container.addEventListener('transitionend', function (e) {
 //     console.log('Look I didz a transition!!!111');
